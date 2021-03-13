d3fb14e073ef702a735fb0d570e80ba4186343e7
Andreas Kasper <djassi@web.de>
2021-03-13 12:53:32 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,11 @@
 <?php
 
-print_r($_SERVER);
+$wait_sec = rand(0,10);
+$set_cookie = md5(microtime(true));
 
+file_put_contents("/var/log/honeypot.test.log", var_export($_SERVER, true), FILE_APPEND);
+
+sleep($wait_sec);
+  
+header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 exit;
