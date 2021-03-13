dbb533f0d4835c027391a2c56a8ec219c0bb17cd
Andreas Kasper <djassi@web.de>
2021-03-13 13:29:11 +0100

    More Infos in log

--- src/index.php
+++ src/index.php
@@ -8,7 +8,15 @@ $set_cookie = md5(microtime(true));
 $row  = date("Y-m-d H:i:s").';';
 $row .= $_SERVER["REMOTE_ADDR"].';';
 $row .= $wait_sec.';';
-@file_put_contents("/var/log/honeypot.log1.csv", $row, FILE_APPEND);
+$row .= '"'.$_SERVER["REQUEST_URI"].'";';
+
+$row .= '"'.json_encode($_SERVER ?? null).'";';
+$row .= '"'.json_encode($_ENV ?? null).'";';
+$row .= '"'.json_encode($_GET ?? null).'";';
+$row .= '"'.json_encode($_POST ?? null).'";';
+$row .= '"'.json_encode($_COOKIE ?? null).'";';
+
+@file_put_contents("/var/log/honeypot.log1.csv", $row.PHP_EOL, FILE_APPEND);
 
 setcookie ("akhp", $set_cookie, time()+365*86400, "/", $_SERVER["HTTP_HOST"], false, false);
 sleep($wait_sec);
