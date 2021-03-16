db7643040d5d7385ae6101ef71b0f53945fe6856
Andreas Kasper <djassi@web.de>
2021-03-16 02:44:10 +0100

    0-20 sec; REQUEST_METHOD

--- src/Honeypot.php
+++ src/Honeypot.php
@@ -6,7 +6,7 @@ class Honeypot {
 
   public static function start() {
   
-    $wait_sec = rand(0,10);
+    $wait_sec = rand(0,20);
     $set_cookie = md5(microtime(true));
     $remote_ip = $_SERVER["HTTP_X_FORWARDED_FOR"] ?? $_SERVER["REMOTE_ADDR"] ?? null;
     
@@ -17,6 +17,8 @@ class Honeypot {
     $row  = date("Y-m-d H:i:s").';';
     $row .= $remote_ip.';';
     $row .= $wait_sec.';';
+    
+    $row .= '"'.($_SERVER["REQUEST_METHOD"] ?? null).'";';
     $row .= '"'.($_SERVER["HTTP_HOST"] ?? null).'";';
     $row .= '"'.($_SERVER["REQUEST_URI"] ?? null).'";';
     $row .= '"'.($ipinfo["result"]["hostname"] ?? null).'";';
