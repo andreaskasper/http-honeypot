0a73cfbadd541088ee5e8d443a016e623b9c6840
Andreas Kasper <djassi@web.de>
2021-03-18 02:51:36 +0100

    Send index-file

--- src/honeypot.go
+++ src/honeypot.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "time"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now()
 	fmt.Println(currentTime.Format("2006-01-02 15:04:05"), " ", r.RemoteAddr, " ", r.URL.Path)
+	if (r.URL.Path == "/") {
+		Openfile, err := os.Open("assets/nginx_default.html")
+		defer Openfile.Close() //Close after function return
+		if err != nil {
+			//File not found, send 404
+			http.Error(writer, "File not found.", 404)
+			return
+		}
+		Openfile.Seek(0, 0)
+		io.Copy(writer, Openfile)
+		return
+	}
 	fmt.Fprintf(w, "Test")
 }
 
