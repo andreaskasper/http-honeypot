09dc7717ee147d4dc1e50cb0da54010437bfee22
Andreas Kasper <djassi@web.de>
2021-03-18 02:32:43 +0100

    Better Console output

--- src/honeypot.go
+++ src/honeypot.go
@@ -4,15 +4,17 @@ import (
     "fmt"
     "log"
     "net/http"
+    "time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-    fmt.Fprintf(w, "Test")
+	currentTime := time.Now()
+	fmt.Println(currentTime.Format("2006.01.02 15:04:05")+" "+r.RemoteAddr+" "+r.URL.Path)
+	fmt.Fprintf(w, "Test")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is listening on port 80")
-	log.Fatal(http.ListenAndServe(":80", nil))
+        http.HandleFunc("/", handler)
+        fmt.Println("Server is listening on port 80")
+        log.Fatal(http.ListenAndServe(":80", nil))
 }
